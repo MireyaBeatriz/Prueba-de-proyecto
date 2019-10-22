b822631236646e14b13a00f93ff53e02b845ddb0
yessyGarcia <yesseniagarcia989@gmail.com>
2019-10-22 11:15:44 -0500

    second commit

---
+++ app/src/main/java/com/example/prubea_de_proyecto/SaludoActivity.java
@@ -0,0 +1,14 @@
+package com.example.prubea_de_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaludoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo);
+    }
+}
